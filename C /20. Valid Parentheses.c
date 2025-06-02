5eed5873fe7044a3bcd331448321751b9aa7d8e2
TechJustice101 <138499851+Huylorddd@users.noreply.github.com>
2025-06-02 11:22:45 +0700

    Update 20. Valid Parentheses.c

--- C /20. Valid Parentheses.c
+++ C /20. Valid Parentheses.c
@@ -5,18 +5,18 @@ bool isValid(char* s) {
     for (int i = 0; s[i] != '\0'; i++) {
         char current_char = s[i];
 
-        if (current_char == '(' || current_char == '[' || current_char == '{'){
-            stack[++top] = current_char;                                              // PUSH: we take the character into stack from bottom to top.
-        }                                                                             // *Pay attention: we use [++top], increase the slot then assign the value.
-        else {
-            if (top == -1) return false;                                              
-
-            char previous_char = stack[top--];                                        // POP: if the input char is 'close bracket' then we assign to the varible and remove the current stack value simultaneously.
-                                                                                      // * [top--], make assignment then decrease.
-            if (current_char == ')' && previous_char != '(' ||                        // >> Check the 'close' bracket whether it is the same type with the 'open'.
-                current_char == ']' && previous_char != '[' ||
-                current_char == '}' && previous_char != '{') return false;
+        if (current_char == '(' || current_char == '[' || current_char == '{'){       //
+            stack[++top] = current_char;                                              // *PUSH: we ONLY take the 'open brackets' into stack from bottom to top.
+        }                                                                             
+        else {                                                                        
+            if (top == -1) return false;                                              // if the 'close brackets' appears before 'open one' -> FALSE statement. E.g: [')'] instead of ['(',')'].                             
+                                                                                      
+            char previous_char = stack[top--];                                        // *POP: if the input char is 'close bracket' then we assign to the varible and remove the current stack value simultaneously.
+                                                                                      
+            if (current_char == ')' && previous_char != '(' ||                        // 
+                current_char == ']' && previous_char != '[' ||                        // check the 'close bracket' whether it is the same type with the 'open' sequentially.
+                current_char == '}' && previous_char != '{') return false;            //
         }
     }
-    return top == -1;                                                                 // return when the stack is empty.
+    return top == -1;                                                                 // return when the stack is empty (nothing is wrong).
 }
