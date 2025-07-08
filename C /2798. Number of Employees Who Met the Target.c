24488056631f269c616b44a94075792d02a7cccb
TechJustice101 <138499851+Huylorddd@users.noreply.github.com>
2025-07-08 10:03:28 +0700

    Create 2798. Number of Employees Who Met the Target.c

---
+++ C /2798. Number of Employees Who Met the Target.c
@@ -0,0 +1,8 @@
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target) {
+    int result = 0;
+    for (int i = 0; i < hoursSize; i++) {
+        if (hours[i] >= target) result++;
+    }
+
+    return result;
+}
