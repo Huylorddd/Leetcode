993b1bc1acd04e32706143ca255d6313078acaff
TechJustice101 <138499851+Huylorddd@users.noreply.github.com>
2025-07-10 14:50:04 +0700

    Update 1389. Create Target Array in the Given Order.c

--- C /1389. Create Target Array in the Given Order.c
+++ C /1389. Create Target Array in the Given Order.c
@@ -4,10 +4,10 @@
 int* createTargetArray(int* nums, int numsSize, int* index, int indexSize, int* returnSize) {
     int* target = calloc(numsSize, sizeof(int));
     if (!target) return NULL;
-    int length = 0;
+    int length = 0;                                     // to check how many elements we're having in the array.
 
     for (int i = 0; i < numsSize; i++) {
-        for (int j = length; j > index[i]; j--) {
+        for (int j = length; j > index[i]; j--) {       // we just need to determine the size of 'current' array (do not use numsSize/indexSize) since it has more memory efficiency!
             target[j] = target[j-1];                    // append nums[i] at index[i] of the 'target' and shift all the right-sided values to the right.
         }
         target[index[i]] = nums[i];
